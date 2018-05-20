1a8b40867df195ab79c29a207267f55b7d43d8cb
crashmatt <uavflightdirector@gmail.com>
2018-05-20 22:04:38 +0200

    Antenna - Enable blending with color alpha

--- src/canvas.cpp
+++ src/canvas.cpp
@@ -121,7 +121,8 @@ void Canvas::paintGL()
 {
 	glClearColor(0.0, 0.0, 0.0, 0.0);
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-	glEnable(GL_DEPTH_TEST);
+    glEnable(GL_DEPTH_TEST | GL_BLEND);
+    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 
 	backdrop->draw();
     foreach(GLMesh* mesh, mesh_list){

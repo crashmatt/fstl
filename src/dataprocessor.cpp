1b302c2a95c0cafc7549a7b8c8b12d5c22ad8ca8
crashmatt <uavflightdirector@gmail.com>
2018-06-05 15:37:39 +0200

    Antenna - Sphere from antenna test data

--- src/dataprocessor.cpp
+++ src/dataprocessor.cpp
@@ -37,57 +37,16 @@ void DataProcessor::process_data(AntennaData &data)
 
 void DataProcessor::build_mesh(AntennaData &data)
 {
-//    std::vector<GLfloat> flat_verts(3 * data.m_z_axis_steps * data.m_x_axis_steps);
+    std::vector<GLfloat> flat_verts(3 * data.m_z_axis_steps * data.m_x_axis_steps);
 
-//    AntennaDataPoint* datapt;
-//    for(int x_step=0; x_step < data.m_x_axis_steps; x_step++){
-//        for(int z_step=0; z_step < data.m_z_axis_steps; z_step++){
-//            int vect_index = 3 * data.data_index(z_step, x_step);
-//            datapt = data.get_antenna_datapoint(x_step, z_step);
-//            Q_ASSERT(datapt != NULL);
-//            float x_theta = cos(datapt->m_rotation.z());
-//            float y_theta = sin(datapt->m_rotation.z());
-//            float x_phi = cos(datapt->m_rotation.x());
-//            float y_phi = sin(datapt->m_rotation.x());
-//            flat_verts[vect_index] = x_theta * x_phi;
-//            flat_verts[vect_index+1] = y_theta * x_phi;
-//            flat_verts[vect_index+2] = y_phi;
-//        }
-//    }
-
-//    int tri_count = (data.m_z_axis_steps-1) * (data.m_x_axis_steps-1) * 2;
-//    // This vector will store triangles as sets of 3 indices
-//    std::vector<GLuint> indices(tri_count*3);
-//    uint index = 0;
-
-//    uint index00, index01, index11, index10;
-//    int x_step = 0;
-////    for(int x_step=0; x_step < data.m_x_axis_steps-1; x_step++){
-//        for(int z_step=0; z_step < data.m_z_axis_steps-1; z_step++){
-//            index00 = data.data_index(x_step, z_step);
-//            index01 = data.data_index(x_step, z_step+1);
-//            index11 = data.data_index(x_step+1, z_step+1);
-//            index10 = data.data_index(x_step+1, z_step);
-//            indices[index+0] = index00;
-//            indices[index+1] = index01;
-//            indices[index+2] = index10;
-////            indices[index+3] = index01;
-////            indices[index+4] = index11;
-////            indices[index+5] = index10;
-//            index += 6;
-//        }
-////    }
-
-    const int axis_steps = 37;
-    std::vector<GLfloat> flat_verts(3 * axis_steps * axis_steps);
-
-    int index = 0;
-    for(int z_step=0; z_step < axis_steps; z_step++){
-        float z_angle = degToRad( (z_step * 180.0 / (axis_steps-1)) - 90.0);
-
-        for(int x_step=0; x_step < axis_steps; x_step++){
-            int vect_index = 3 * index;
-            float x_angle = degToRad( (x_step * 360.0 / (axis_steps-1)) - 180.0);
+    AntennaDataPoint* datapt;
+    for(int z_step=0; z_step < data.m_z_axis_steps; z_step++){
+        for(int x_step=0; x_step < data.m_x_axis_steps; x_step++){
+            datapt = data.get_antenna_datapoint(x_step, z_step);
+            Q_ASSERT(datapt != NULL);
+            float x_angle = degToRad(datapt->m_rotation.x());
+            float z_angle = degToRad(datapt->m_rotation.z());
+            int vect_index = 3 * data.data_index(z_step, x_step);
             float x_theta = sin(z_angle);
             float y_theta = cos(z_angle);
             float x_phi = cos(x_angle);
@@ -95,23 +54,21 @@ void DataProcessor::build_mesh(AntennaData &data)
             flat_verts[vect_index] =  y_theta * x_phi;
             flat_verts[vect_index+1] = x_theta * x_phi;
             flat_verts[vect_index+2] = z_phi;
-            index++;
         }
     }
 
-    int tri_count = (axis_steps) * (axis_steps) * 2;
+    int tri_count = (data.m_z_axis_steps) * (data.m_x_axis_steps) * 2;
     // This vector will store triangles as sets of 3 indices
     std::vector<GLuint> indices(tri_count*3);
-    index = 0;
 
+    uint index = 0;
     uint index00, index01, index11, index10;
-    index = 0;
-    for(int x_step=0; x_step < axis_steps; x_step++){
-        for(int z_step=0; z_step < axis_steps-1; z_step++){
-            index00 = (z_step)  +((x_step)  *axis_steps);
-            index01 = (z_step+1)+((x_step)  *axis_steps);
-            index11 = (z_step+1)+((x_step+1)*axis_steps);
-            index10 = (z_step)  +((x_step+1)*axis_steps);
+    for(int z_step=0; z_step < data.m_z_axis_steps; z_step++){
+        for(int x_step=0; x_step < data.m_x_axis_steps-1; x_step++){
+            index00 = data.data_index(z_step, x_step);
+            index01 = data.data_index(z_step+1, x_step);
+            index11 = data.data_index(z_step+1, x_step+1);
+            index10 = data.data_index(z_step, x_step+1);
             indices[index+0] = index00;
             indices[index+1] = index01;
             indices[index+2] = index10;
